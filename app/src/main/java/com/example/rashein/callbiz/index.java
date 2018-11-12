c5d384014b9ade17d11ec94282074505a0bceae4
amanvaidya <amanvaidya700@gmail.com>
2018-11-12 22:02:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rashein/callbiz/index.java
@@ -0,0 +1,22 @@
+package com.example.rashein.callbiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class index extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+        //mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        webView = (WebView) findViewById(R.id.activity_main_webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://callbiznow.in/");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setPluginState(WebSettings.PluginState.ON);
+    }
+}
