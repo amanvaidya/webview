93019a1746bf1b598cde145f0bd6af070765e53f
aman <amanvaidya700@gmail.com>
2018-11-16 11:35:39 +0530

    Update index.java

--- app/src/main/java/com/example/rashein/callbiz/index.java
+++ app/src/main/java/com/example/rashein/callbiz/index.java
@@ -1,9 +1,13 @@
-package com.example.rashein.callbiz;
+package com.example.aman.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 
 public class index extends AppCompatActivity {
     private WebView webView;
@@ -11,13 +15,21 @@ public class index extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
-        //mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
         webView = (WebView) findViewById(R.id.activity_main_webview);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://google.com/");
+        webView.loadUrl("http://callbiznow.in/");
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
         webSettings.setPluginState(WebSettings.PluginState.ON);
+        webView = new WebView(this);
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result){
+                return super.onJsAlert(view,url,message,result);
+            }
+        });
     }
 }
